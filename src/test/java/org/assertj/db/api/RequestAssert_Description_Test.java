878e1cb1e1325d388b0a5a3ff2370b9f097a82bf
regis1512 <regis1512@gmail.com>
2014-06-26 21:29:56 +0200

    Fix the tests on private fields by using FieldSupport class from assertj-core

--- src/test/java/org/assertj/db/api/RequestAssert_Description_Test.java
+++ src/test/java/org/assertj/db/api/RequestAssert_Description_Test.java
@@ -3,11 +3,10 @@ package org.assertj.db.api;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.db.api.Assertions.assertThat;
 
-import java.lang.reflect.Field;
-
 import org.assertj.core.api.WritableAssertionInfo;
 import org.assertj.core.description.Description;
 import org.assertj.core.description.TextDescription;
+import org.assertj.core.util.introspection.FieldSupport;
 import org.assertj.db.common.AbstractTest;
 import org.assertj.db.type.Request;
 import org.junit.Test;
@@ -22,47 +21,31 @@ public class RequestAssert_Description_Test extends AbstractTest {
 
   /**
    * This method tests the result of description from text and parameters.
-   * 
-   * @throws SecurityException
-   * @throws NoSuchFieldException
-   * @throws IllegalArgumentException
-   * @throws IllegalAccessException
    */
   @Test
-  public void test_description_by_text() throws SecurityException, NoSuchFieldException, IllegalArgumentException,
-      IllegalAccessException {
+  public void test_description_by_text() {
 
     Request request = new Request(source, "select * from movie");
-
     RequestAssert assertion = assertThat(request).as("test %s", "param");
 
-    Field field = AbstractDbAssert.class.getDeclaredField("info");
-    field.setAccessible(true);
-    WritableAssertionInfo info = (WritableAssertionInfo) field.get(assertion);
+    FieldSupport.setAllowExtractingPrivateFields(true);
+    WritableAssertionInfo info = FieldSupport.instance().fieldValue("info", WritableAssertionInfo.class, assertion);
 
     assertThat(info.descriptionText()).isEqualTo("test param");
   }
 
   /**
    * This method tests the result of description from description object.
-   * 
-   * @throws SecurityException
-   * @throws NoSuchFieldException
-   * @throws IllegalArgumentException
-   * @throws IllegalAccessException
    */
   @Test
-  public void test_description_by_description() throws SecurityException, NoSuchFieldException, IllegalArgumentException,
-      IllegalAccessException {
+  public void test_description_by_description() {
 
     Request request = new Request(source, "select * from movie");
-
     Description description = new TextDescription("test %s %s", "param", "param2");
     RequestAssert assertion = assertThat(request).as(description);
 
-    Field field = AbstractDbAssert.class.getDeclaredField("info");
-    field.setAccessible(true);
-    WritableAssertionInfo info = (WritableAssertionInfo) field.get(assertion);
+    FieldSupport.setAllowExtractingPrivateFields(true);
+    WritableAssertionInfo info = FieldSupport.instance().fieldValue("info", WritableAssertionInfo.class, assertion);
 
     assertThat(info.descriptionText()).isEqualTo("test param param2");
   }
--- src/test/java/org/assertj/db/api/TableAssert_Description_Test.java
+++ src/test/java/org/assertj/db/api/TableAssert_Description_Test.java
@@ -3,11 +3,10 @@ package org.assertj.db.api;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.db.api.Assertions.assertThat;
 
-import java.lang.reflect.Field;
-
 import org.assertj.core.api.WritableAssertionInfo;
 import org.assertj.core.description.Description;
 import org.assertj.core.description.TextDescription;
+import org.assertj.core.util.introspection.FieldSupport;
 import org.assertj.db.common.AbstractTest;
 import org.assertj.db.type.Table;
 import org.junit.Test;
@@ -22,47 +21,31 @@ public class TableAssert_Description_Test extends AbstractTest {
 
   /**
    * This method tests the result of description from text and parameters.
-   * 
-   * @throws SecurityException
-   * @throws NoSuchFieldException
-   * @throws IllegalArgumentException
-   * @throws IllegalAccessException
    */
   @Test
-  public void test_description_by_text() throws SecurityException, NoSuchFieldException, IllegalArgumentException,
-      IllegalAccessException {
+  public void test_description_by_text() {
 
     Table table = new Table(source, "movie");
-
     TableAssert assertion = assertThat(table).as("test %s", "param");
 
-    Field field = AbstractDbAssert.class.getDeclaredField("info");
-    field.setAccessible(true);
-    WritableAssertionInfo info = (WritableAssertionInfo) field.get(assertion);
+    FieldSupport.setAllowExtractingPrivateFields(true);
+    WritableAssertionInfo info = FieldSupport.instance().fieldValue("info", WritableAssertionInfo.class, assertion);
 
     assertThat(info.descriptionText()).isEqualTo("test param");
   }
 
   /**
    * This method tests the result of description from description object.
-   * 
-   * @throws SecurityException
-   * @throws NoSuchFieldException
-   * @throws IllegalArgumentException
-   * @throws IllegalAccessException
    */
   @Test
-  public void test_description_by_description() throws SecurityException, NoSuchFieldException, IllegalArgumentException,
-      IllegalAccessException {
+  public void test_description_by_description() {
 
     Table table = new Table(source, "movie");
-
     Description description = new TextDescription("test %s %s", "param", "param2");
     TableAssert assertion = assertThat(table).as(description);
 
-    Field field = AbstractDbAssert.class.getDeclaredField("info");
-    field.setAccessible(true);
-    WritableAssertionInfo info = (WritableAssertionInfo) field.get(assertion);
+    FieldSupport.setAllowExtractingPrivateFields(true);
+    WritableAssertionInfo info = FieldSupport.instance().fieldValue("info", WritableAssertionInfo.class, assertion);
 
     assertThat(info.descriptionText()).isEqualTo("test param param2");
   }
