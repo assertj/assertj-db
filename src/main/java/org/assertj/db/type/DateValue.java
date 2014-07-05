f1165c04b7f7315ce05c149178bf0b68d22f150e
regis1512 <regis1512@gmail.com>
2014-07-05 23:14:48 +0200

    Add date value

---
+++ src/main/java/org/assertj/db/type/DateValue.java
@@ -0,0 +1,125 @@
+package org.assertj.db.type;
+
+import static java.lang.Character.isDigit;
+
+import java.text.ParseException;
+
+/**
+ * This class represents a date value in the dabatase.
+ * 
+ * @author Régis Pouiller
+ * 
+ */
+public class DateValue {
+
+  /**
+   * Day of the month.
+   */
+  private int dayOfTheMonth;
+  /**
+   * Month.
+   */
+  private int month;
+  /**
+   * Year.
+   */
+  private int year;
+  /**
+   * Indicates where there are the digits in the {@code String} for {@link DateValue#DateValue(String)}.
+   */
+  private boolean[] DATE_FORMAT = { true, true, true, true, false, true, true, false, true, true };
+
+  /**
+   * Makes an instance of date value from a day of month, a month and an year.
+   * 
+   * @param year Year.
+   * @param month Month.
+   * @param dayOfTheMonth Day of the month.
+   */
+  public static DateValue of(int year, int month, int dayOfTheMonth) {
+    return new DateValue(year, month, dayOfTheMonth);
+  }
+
+  /**
+   * Makes an instance of date value from a {@code String} in format {@code yyyy-mm-dd}.
+   * 
+   * @param date Date in {@code String} format ({@code yyyy-mm-dd}).
+   * @throws NullPointerException If {@code date} is {@code null}.
+   * @throws ParseException If {@code date} don't respect the {@code yyyy-mm-dd} format.
+   */
+  public static DateValue parse(String date) throws ParseException {
+    return new DateValue(date);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param year Year.
+   * @param month Month.
+   * @param dayOfTheMonth Day of the month.
+   */
+  public DateValue(int year, int month, int dayOfTheMonth) {
+    this.dayOfTheMonth = dayOfTheMonth;
+    this.month = month;
+    this.year = year;
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param date Date in {@code String} format ({@code yyyy-mm-dd}).
+   * @throws NullPointerException If {@code date} is {@code null}.
+   * @throws ParseException If {@code date} don't respect the {@code yyyy-mm-dd} format.
+   */
+  public DateValue(String date) throws ParseException {
+    if (date == null) {
+      throw new NullPointerException("date should be not null");
+    }
+    if (date.length() != DATE_FORMAT.length) {
+      throw new ParseException("date must be of " + DATE_FORMAT.length + " characters", date.length());
+    }
+    for (int i=0; i < DATE_FORMAT.length; i++) {
+      if (DATE_FORMAT[i]) {
+        if (!isDigit(date.charAt(i))) {
+          throw new ParseException("date must respect yyyy-mm-dd format", i);
+        }
+      }
+      else {
+        if (date.charAt(i) != '-') {
+          throw new ParseException("date must respect yyyy-mm-dd format", i);
+        }
+      }
+    }
+
+    year = Integer.parseInt(date.substring(0, 4));
+    month = Integer.parseInt(date.substring(5, 7));
+    dayOfTheMonth = Integer.parseInt(date.substring(8));
+  }
+
+  /**
+   * Returns the day of the month.
+   * 
+   * @return The day of the month.
+   */
+  public int getDayOfTheMonth() {
+    return dayOfTheMonth;
+  }
+
+  /**
+   * Returns the month.
+   * 
+   * @return The month.
+   */
+  public int getMonth() {
+    return month;
+  }
+
+  /**
+   * Returns the year.
+   * 
+   * @return The year.
+   */
+  public int getYear() {
+    return year;
+  }
+}
---
+++ src/test/java/org/assertj/db/type/DateValue_Test.java
@@ -0,0 +1,127 @@
+package org.assertj.db.type;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.text.ParseException;
+
+import org.assertj.db.common.AbstractTest;
+import org.junit.Test;
+
+/**
+ * Tests on the date value.
+ * 
+ * @author Régis Pouiller
+ * 
+ */
+public class DateValue_Test extends AbstractTest {
+
+  /**
+   * This method tests the constructor with a {@code String}.
+   */
+  @Test
+  public void test_contructor_with_string() throws ParseException {
+    DateValue dateValue = new DateValue("2007-12-23");
+    assertThat(dateValue.getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateValue.getMonth()).isEqualTo(12);
+    assertThat(dateValue.getYear()).isEqualTo(2007);
+  }
+
+  /**
+   * This method should throw a {@code NullPointerException} because passing a {@code null} parameter to constructor
+   * with a {@code String}.
+   */
+  @Test(expected = NullPointerException.class)
+  public void should_constructor_with_string_fail_if_date_is_null() throws ParseException {
+    new DateValue(null);
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad length.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_length() throws ParseException {
+    new DateValue("12345678901");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad character on year.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_character_on_year() throws ParseException {
+    new DateValue("a007-12-23");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad character on separator.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_character_on_separator() throws ParseException {
+    new DateValue("2007a12-23");
+  }
+
+  /**
+   * This method tests the constructor with values (3 {@code int}).
+   */
+  @Test
+  public void test_contructor_with_values() throws ParseException {
+    DateValue dateValue = new DateValue(2007, 12, 23);
+    assertThat(dateValue.getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateValue.getMonth()).isEqualTo(12);
+    assertThat(dateValue.getYear()).isEqualTo(2007);
+  }
+
+  /**
+   * This method tests the {@code parse} static method.
+   */
+  @Test
+  public void test_parse() throws ParseException {
+    DateValue dateValue = DateValue.parse("2002-07-26");
+    assertThat(dateValue.getDayOfTheMonth()).isEqualTo(26);
+    assertThat(dateValue.getMonth()).isEqualTo(07);
+    assertThat(dateValue.getYear()).isEqualTo(2002);
+  }
+
+  /**
+   * This method should throw a {@code NullPointerException} because passing a {@code null} parameter to the
+   * {@code parse} static method.
+   */
+  @Test(expected = NullPointerException.class)
+  public void should_parse_fail_if_date_is_null() throws ParseException {
+    DateValue.parse(null);
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad length.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_length() throws ParseException {
+    DateValue.parse("12345678901");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad character on year.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_character_on_year() throws ParseException {
+    DateValue.parse("a007-12-23");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad character on separator.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_character_on_separator() throws ParseException {
+    DateValue.parse("2007a12-23");
+  }
+
+  /**
+   * This method tests the {@code of} static method.
+   */
+  @Test
+  public void test_of() throws ParseException {
+    DateValue dateValue = DateValue.of(2002, 7, 26);
+    assertThat(dateValue.getDayOfTheMonth()).isEqualTo(26);
+    assertThat(dateValue.getMonth()).isEqualTo(07);
+    assertThat(dateValue.getYear()).isEqualTo(2002);
+  }
+}
