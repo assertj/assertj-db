e69566e75c645cf7a06ed3aa2e06b81da74af1aa
regis1512 <regis1512@gmail.com>
2014-07-06 20:58:37 +0200

    Add date/time value

---
+++ src/main/java/org/assertj/db/type/DateTimeValue.java
@@ -0,0 +1,126 @@
+package org.assertj.db.type;
+
+import java.text.ParseException;
+
+/**
+ * This class represents a date/time value in the database.
+ * 
+ * @author Régis Pouiller
+ * 
+ */
+public class DateTimeValue {
+
+  /**
+   * The date part.
+   */
+  private final DateValue date;
+  /**
+   * The time part.
+   */
+  private final TimeValue time;
+  /**
+   * Indicates where there are the digits in the {@code String} for {@link DateValue#DateValue(String)}.
+   */
+  private static final String DATE_FORMAT = "\\d\\d\\d\\d-\\d\\d-\\d\\d";
+  /**
+   * Indicates where there are the digits for {@code String} for {@link TimeValue#TimeValue(String)}.
+   */
+  private static final String TIME_FORMAT = "\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d";
+  /**
+   * Indicates where there are the digits in style with seconds for {@code String} for
+   * {@link TimeValue#TimeValue(String)}.
+   */
+  private static final String TIME_FORMAT_WITH_SECONDS = "\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d";
+  /**
+   * Indicates where there are the digits in style with nanoseconds for {@code String} for
+   * {@link TimeValue#TimeValue(String)}.
+   */
+  private static final String TIME_FORMAT_WITH_NANO = "\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d.\\d\\d\\d\\d\\d\\d\\d\\d\\d";
+
+  /**
+   * Makes an instance of date/time value from a date and a time.
+   * 
+   * @param date The date.
+   * @param time The time.
+   * @return An instance of date/time.
+   */
+  public static DateTimeValue of(DateValue date, TimeValue time) {
+    return new DateTimeValue(date, time);
+  }
+
+  /**
+   * Makes an instance of date/time value from a {@code String} in {@code yyyy-mm-dd}, {@code yyyy-mm-dd hh:mm},
+   * {@code yyyy-mm-dd hh:mm:ss} or {@code yyyy-mm-dd hh:mm:ss.nnnnnnnnn} format.
+   * 
+   * @param dateTime Date/time in {@code String} format ({@code yyyy-mm-dd}).
+   * @throws NullPointerException If {@code dateTime} is {@code null}.
+   * @throws ParseException If {@code date} don't respect the {@code yyyy-mm-dd}, {@code yyyy-mm-dd hh:mm},
+   *           {@code yyyy-mm-dd hh:mm:ss} or {@code yyyy-mm-dd hh:mm:ss.nnnnnnnnn} format.
+   * @return An instance of date/time value.
+   */
+  public static DateTimeValue parse(String dateTime) throws ParseException {
+    return new DateTimeValue(dateTime);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param date The date.
+   * @param time The time.
+   */
+  public DateTimeValue(DateValue date, TimeValue time) {
+    this.date = date;
+    this.time = time;
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param dateTime Time in {@code String} format ({@code yyyy-mm-dd}, {@code yyyy-mm-dd hh:mm},
+   *          {@code yyyy-mm-dd hh:mm:ss} or {@code yyyy-mm-dd hh:mm:ss.nnnnnnnnn}).
+   * @throws NullPointerException If {@code dateTime} is {@code null}.
+   * @throws ParseException If {@code date} don't respect the {@code yyyy-mm-dd}, {@code yyyy-mm-dd hh:mm},
+   *           {@code yyyy-mm-dd hh:mm:ss} or {@code yyyy-mm-dd hh:mm:ss.nnnnnnnnn} format.
+   */
+  public DateTimeValue(String dateTime) throws ParseException {
+    if (dateTime == null) {
+      throw new NullPointerException("date/time should be not null");
+    }
+
+    if (dateTime.matches(DATE_FORMAT)) {
+      date = DateValue.parse(dateTime);
+      time = new TimeValue(0, 0);
+    } else if (dateTime.matches(TIME_FORMAT)) {
+      date = DateValue.parse(dateTime.substring(0, 10));
+      time = TimeValue.parse(dateTime.substring(11));
+    } else if (dateTime.matches(TIME_FORMAT_WITH_SECONDS)) {
+      date = DateValue.parse(dateTime.substring(0, 10));
+      time = TimeValue.parse(dateTime.substring(11));
+    } else if (dateTime.matches(TIME_FORMAT_WITH_NANO)) {
+      date = DateValue.parse(dateTime.substring(0, 10));
+      time = TimeValue.parse(dateTime.substring(11));
+    } else {
+      throw new ParseException("date/time must respect yyyy-mm-dd, yyyy-mm-dd hh:mm, "
+          + "yyyy-mm-dd hh:mm:ss or yyyy-mm-dd hh:mm:ss.nnnnnnnnn format", dateTime.length());
+    }
+  }
+
+  /**
+   * Returns the date.
+   * 
+   * @return The date.
+   */
+  public DateValue getDate() {
+    return date;
+  }
+
+  /**
+   * Returns the time.
+   * 
+   * @return The time.
+   */
+  public TimeValue getTime() {
+    return time;
+  }
+
+}
---
+++ src/test/java/org/assertj/db/type/DateTimeValue_Test.java
@@ -0,0 +1,243 @@
+package org.assertj.db.type;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.text.ParseException;
+
+import org.assertj.db.common.AbstractTest;
+import org.junit.Test;
+
+/**
+ * Tests on the date/time value.
+ * 
+ * @author Régis Pouiller
+ * 
+ */
+public class DateTimeValue_Test extends AbstractTest {
+
+  /**
+   * This method tests the constructor with a {@code String} with only the day.
+   */
+  @Test
+  public void test_contructor_with_string_only_day() throws ParseException {
+    DateTimeValue dateTimeValue = new DateTimeValue("2007-12-23");
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(0);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(0);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(0);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the constructor with a {@code String} with minutes.
+   */
+  @Test
+  public void test_contructor_with_string_with_minutes() throws ParseException {
+    DateTimeValue dateTimeValue = new DateTimeValue("2007-12-23 09:01");
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(9);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(1);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(0);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the constructor with a {@code String} with seconds.
+   */
+  @Test
+  public void test_contructor_with_string_with_seconds() throws ParseException {
+    DateTimeValue dateTimeValue = new DateTimeValue("2007-12-23 09:01:06");
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(9);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(1);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(6);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the constructor with a {@code String} with nanoseconds.
+   */
+  @Test
+  public void test_contructor_with_string_with_nano() throws ParseException {
+    DateTimeValue dateTimeValue = new DateTimeValue("2007-12-23 09:01:06.000000003");
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(9);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(1);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(6);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(3);
+  }
+
+  /**
+   * This method should throw a {@code NullPointerException} because passing a {@code null} parameter to constructor
+   * with a {@code String}.
+   */
+  @Test(expected = NullPointerException.class)
+  public void should_constructor_with_string_fail_if_date_is_null() throws ParseException {
+    new DateTimeValue(null);
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad length.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_length() throws ParseException {
+    new DateTimeValue("12345678901");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad character
+   * on year.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_character_on_year() throws ParseException {
+    new DateTimeValue("a007-12-23");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad character
+   * on separator.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_character_on_separator() throws ParseException {
+    new DateTimeValue("2007a12-23");
+  }
+
+  /**
+   * This method tests the constructor with values.
+   */
+  @Test
+  public void test_contructor_with_values() throws ParseException {
+    DateValue dateValue = new DateValue(2007, 12, 23);
+    TimeValue timeValue = new TimeValue(9, 1, 6, 3);
+    DateTimeValue dateTimeValue = new DateTimeValue(dateValue, timeValue);
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(9);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(1);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(6);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(3);
+  }
+
+  /**
+   * This method tests the {@code parse} static method with a {@code String} with only the day.
+   */
+  @Test
+  public void test_parse_only_day() throws ParseException {
+    DateTimeValue dateTimeValue = DateTimeValue.parse("2007-12-23");
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(0);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(0);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(0);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the {@code parse} static method with a {@code String} with minutes.
+   */
+  @Test
+  public void test_parse_with_minutes() throws ParseException {
+    DateTimeValue dateTimeValue = DateTimeValue.parse("2007-12-23 09:01");
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(9);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(1);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(0);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the {@code parse} static method with a {@code String} with seconds.
+   */
+  @Test
+  public void test_parse_with_seconds() throws ParseException {
+    DateTimeValue dateTimeValue = DateTimeValue.parse("2007-12-23 09:01:06");
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(9);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(1);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(6);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the {@code parse} static method with a {@code String} with nanoseconds.
+   */
+  @Test
+  public void test_parse_with_nano() throws ParseException {
+    DateTimeValue dateTimeValue = DateTimeValue.parse("2007-12-23 09:01:06.000000003");
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(9);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(1);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(6);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(3);
+  }
+
+  /**
+   * This method should throw a {@code NullPointerException} because passing a {@code null} parameter to the
+   * {@code parse} static method.
+   */
+  @Test(expected = NullPointerException.class)
+  public void should_parse_fail_if_date_is_null() throws ParseException {
+    DateTimeValue.parse(null);
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to {@code parse} static method with a
+   * bad length.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_length() throws ParseException {
+    DateTimeValue.parse("12345678901");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to {@code parse} static method with a
+   * bad character on year.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_character_on_year() throws ParseException {
+    DateTimeValue.parse("a007-12-23");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to {@code parse} static method with a
+   * bad character on separator.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_character_on_separator() throws ParseException {
+    DateTimeValue.parse("2007a12-23");
+  }
+
+  /**
+   * This method tests the {@code of} static method.
+   */
+  @Test
+  public void test_of() throws ParseException {
+    DateValue dateValue = DateValue.of(2007, 12, 23);
+    TimeValue timeValue = TimeValue.of(9, 1, 6, 3);
+    DateTimeValue dateTimeValue = DateTimeValue.of(dateValue, timeValue);
+    assertThat(dateTimeValue.getDate().getDayOfTheMonth()).isEqualTo(23);
+    assertThat(dateTimeValue.getDate().getMonth()).isEqualTo(12);
+    assertThat(dateTimeValue.getDate().getYear()).isEqualTo(2007);
+    assertThat(dateTimeValue.getTime().getHour()).isEqualTo(9);
+    assertThat(dateTimeValue.getTime().getMinutes()).isEqualTo(1);
+    assertThat(dateTimeValue.getTime().getSeconds()).isEqualTo(6);
+    assertThat(dateTimeValue.getTime().getNanoSeconds()).isEqualTo(3);
+  }
+
+}
