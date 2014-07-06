4a781557f33359eccb3a1964644a45949a6bd4ed
regis1512 <regis1512@gmail.com>
2014-07-06 18:37:16 +0200

    Add time value

---
+++ src/main/java/org/assertj/db/type/TimeValue.java
@@ -0,0 +1,229 @@
+package org.assertj.db.type;
+
+import java.text.ParseException;
+
+public class TimeValue {
+
+  /**
+   * Hour.
+   */
+  private final int hour;
+  /**
+   * Minutes.
+   */
+  private final int minutes;
+  /**
+   * Seconds.
+   */
+  private final int seconds;
+  /**
+   * Nanoseconds.
+   */
+  private final int nanoSeconds;
+  /**
+   * Indicates where there are the digits for {@code String} for {@link TimeValue#TimeValue(String)}.
+   */
+  private static final String DATE_FORMAT = "\\d\\d:\\d\\d";
+  /**
+   * Indicates where there are the digits in style with seconds for {@code String} for
+   * {@link TimeValue#TimeValue(String)}.
+   */
+  private static final String DATE_FORMAT_WITH_SECONDS = "\\d\\d:\\d\\d:\\d\\d";
+  /**
+   * Indicates where there are the digits in style with nanoseconds for {@code String} for
+   * {@link TimeValue#TimeValue(String)}.
+   */
+  private static final String DATE_FORMAT_WITH_NANO = "\\d\\d:\\d\\d:\\d\\d.\\d\\d\\d\\d\\d\\d\\d\\d\\d";
+
+  /**
+   * Makes an instance of time value from a hour, minutes, seconds and nanoseconds.
+   * 
+   * @param hour Hour.
+   * @param minutes Minutes.
+   * @param seconds Seconds.
+   * @param nanoSeconds Nanoseconds.
+   */
+  public static TimeValue of(int hour, int minutes, int seconds, int nanoSeconds) {
+    return new TimeValue(hour, minutes, seconds, nanoSeconds);
+  }
+
+  /**
+   * Makes an instance of time value from a hour, minutes and seconds.
+   * 
+   * @param hour Hour.
+   * @param minutes Minutes.
+   * @param seconds Seconds.
+   */
+  public static TimeValue of(int hour, int minutes, int seconds) {
+    return new TimeValue(hour, minutes, seconds);
+  }
+
+  /**
+   * Makes an instance of time value from a hour and minutes.
+   * 
+   * @param hour Hour.
+   * @param minutes Minutes.
+   */
+  public static TimeValue of(int hour, int minutes) {
+    return new TimeValue(hour, minutes);
+  }
+
+  /**
+   * Makes an instance of time value from a {@code String} in {@code hh:mm}, {@code hh:mm:ss} or
+   * {@code hh:mm:ss.nnnnnnnnn} format.
+   * 
+   * @param time Time in {@code String} format ({@code hh:mm}, {@code hh:mm:ss} or {@code hh:mm:ss.nnnnnnnnn}).
+   * @throws NullPointerException If {@code time} is {@code null}.
+   * @throws ParseException If {@code time} don't respect the {@code hh:mm}, {@code hh:mm:ss} or
+   *           {@code hh:mm:ss.nnnnnnnnn} format.
+   */
+  public static TimeValue parse(String date) throws ParseException {
+    return new TimeValue(date);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param hour Hour.
+   * @param minutes Minutes.
+   * @param seconds Seconds.
+   * @param nanoSeconds Nanoseconds.
+   */
+  public TimeValue(int hour, int minutes, int seconds, int nanoSeconds) {
+    this.hour = hour;
+    this.minutes = minutes;
+    this.seconds = seconds;
+    this.nanoSeconds = nanoSeconds;
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param hour Hour.
+   * @param minutes Minutes.
+   * @param seconds Seconds.
+   */
+  public TimeValue(int hour, int minutes, int seconds) {
+    this(hour, minutes, seconds, 0);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param hour Hour.
+   * @param minutes Minutes.
+   */
+  public TimeValue(int hour, int minutes) {
+    this(hour, minutes, 0, 0);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param time Time in {@code String} format ({@code hh:mm}, {@code hh:mm:ss} or {@code hh:mm:ss.nnnnnnnnn}).
+   * @throws NullPointerException If {@code time} is {@code null}.
+   * @throws ParseException If {@code time} don't respect the {@code hh:mm}, {@code hh:mm:ss} or
+   *           {@code hh:mm:ss.nnnnnnnnn} format.
+   */
+  public TimeValue(String time) throws ParseException {
+    if (time == null) {
+      throw new NullPointerException("time should be not null");
+    }
+
+    if (time.matches(DATE_FORMAT)) {
+      hour = Integer.parseInt(time.substring(0, 2));
+      minutes = Integer.parseInt(time.substring(3));
+      seconds = 0;
+      nanoSeconds = 0;
+    } else if (time.matches(DATE_FORMAT_WITH_SECONDS)) {
+      hour = Integer.parseInt(time.substring(0, 2));
+      minutes = Integer.parseInt(time.substring(3, 5));
+      seconds = Integer.parseInt(time.substring(6));
+      nanoSeconds = 0;
+    } else if (time.matches(DATE_FORMAT_WITH_NANO)) {
+      hour = Integer.parseInt(time.substring(0, 2));
+      minutes = Integer.parseInt(time.substring(3, 5));
+      seconds = Integer.parseInt(time.substring(6, 8));
+      nanoSeconds = Integer.parseInt(time.substring(9));
+    } else {
+      throw new ParseException("time must respect hh:mm, hh:mm:ss or hh:mm:ss.nnnnnnnnn format", time.length());
+    }
+
+    // if (time.length() == DATE_FORMAT.length) {
+    // for (int i = 0; i < DATE_FORMAT_PART_1.length; i++) {
+    // if (DATE_FORMAT_PART_1[i]) {
+    // if (!isDigit(time.charAt(i))) {
+    // throw new ParseException("time must respect hh:mm", i);
+    // }
+    // } else {
+    // if (time.charAt(i) != ':') {
+    // throw new ParseException("time must respect hh:mm", i);
+    // }
+    // }
+    // }
+    // } else if (time.length() == DATE_FORMAT_PART_1.length + DATE_FORMAT_PART_2.length) {
+    // for (int i = 0; i < DATE_FORMAT_PART_1.length; i++) {
+    // if (DATE_FORMAT_PART_1[i]) {
+    // if (!isDigit(time.charAt(i))) {
+    // throw new ParseException("time must respect hh:mm:ss", i);
+    // }
+    // } else {
+    // if (time.charAt(i) != ':') {
+    // throw new ParseException("time must respect hh:mm:ss", i);
+    // }
+    // }
+    // }
+    // for (int i = 0; i < DATE_FORMAT_PART_2.length; i++) {
+    // if (DATE_FORMAT_PART_2[i]) {
+    // if (!isDigit(time.charAt(DATE_FORMAT_PART_1.length + i))) {
+    // throw new ParseException("time must respect hh:mm:ss", DATE_FORMAT_PART_1.length + i);
+    // }
+    // } else {
+    // if (time.charAt(DATE_FORMAT_PART_1.length + i) != ':') {
+    // throw new ParseException("time must respect hh:mm:ss", DATE_FORMAT_PART_1.length + i);
+    // }
+    // }
+    // }
+    // } else if (time.length() == DATE_FORMAT_PART_1.length + DATE_FORMAT_PART_2.length + DATE_FORMAT_PART_3.length) {
+    //
+    // }
+
+  }
+
+  /**
+   * Returns the hour.
+   * 
+   * @return The hour.
+   */
+  public int getHour() {
+    return hour;
+  }
+
+  /**
+   * Returns the minutes.
+   * 
+   * @return The minutes.
+   */
+  public int getMinutes() {
+    return minutes;
+  }
+
+  /**
+   * Returns the seconds.
+   * 
+   * @return The seconds.
+   */
+  public int getSeconds() {
+    return seconds;
+  }
+
+  /**
+   * Returns the nanoseconds.
+   * 
+   * @return The nanoseconds.
+   */
+  public int getNanoSeconds() {
+    return nanoSeconds;
+  }
+
+}
---
+++ src/test/java/org/assertj/db/type/TimeValue_Test.java
@@ -0,0 +1,228 @@
+package org.assertj.db.type;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.text.ParseException;
+
+import org.assertj.db.common.AbstractTest;
+import org.junit.Test;
+
+/**
+ * Tests on the time value.
+ * 
+ * @author Régis Pouiller
+ * 
+ */
+public class TimeValue_Test extends AbstractTest {
+
+  /**
+   * This method tests the constructor with a {@code String} containing nanoseconds.
+   */
+  @Test
+  public void test_contructor_with_string_containing_nanoseconds() throws ParseException {
+    TimeValue timeValue = new TimeValue("09:01:06.000000003");
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(6);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(3);
+  }
+
+  /**
+   * This method tests the constructor with a {@code String} containing seconds.
+   */
+  @Test
+  public void test_contructor_with_string_containing_seconds() throws ParseException {
+    TimeValue timeValue = new TimeValue("09:01:06");
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(6);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the constructor with a {@code String} containing minutes.
+   */
+  @Test
+  public void test_contructor_with_string_containing_minutes() throws ParseException {
+    TimeValue timeValue = new TimeValue("09:01");
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(0);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method should throw a {@code NullPointerException} because passing a {@code null} parameter to constructor
+   * with a {@code String}.
+   */
+  @Test(expected = NullPointerException.class)
+  public void should_constructor_with_string_fail_if_date_is_null() throws ParseException {
+    new TimeValue(null);
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad length.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_length() throws ParseException {
+    new TimeValue("12345678901");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad character on year.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_character_on_year() throws ParseException {
+    new TimeValue("a9:01");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to constructor with a bad character on separator.
+   */
+  @Test(expected = ParseException.class)
+  public void should_constructor_with_string_fail_if_date_have_bad_character_on_separator() throws ParseException {
+    new TimeValue("09a01");
+  }
+
+  /**
+   * This method tests the constructor with values (4 {@code int}).
+   */
+  @Test
+  public void test_contructor_with_four_values() throws ParseException {
+    TimeValue timeValue = new TimeValue(9, 1, 6, 3);
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(6);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(3);
+  }
+
+  /**
+   * This method tests the constructor with values (3 {@code int}).
+   */
+  @Test
+  public void test_contructor_with_three_values() throws ParseException {
+    TimeValue timeValue = new TimeValue(9, 1, 6);
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(6);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the constructor with values (2 {@code int}).
+   */
+  @Test
+  public void test_contructor_with_two_values() throws ParseException {
+    TimeValue timeValue = new TimeValue(9, 1);
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(0);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the {@code parse} static method with a {@code String} containing nanoseconds.
+   */
+  @Test
+  public void test_parse_containing_nanoseconds() throws ParseException {
+    TimeValue timeValue = TimeValue.parse("09:01:06.000000003");
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(6);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(3);
+  }
+
+  /**
+   * This method tests the {@code parse} static method with a {@code String} containing seconds.
+   */
+  @Test
+  public void test_parse_containing_seconds() throws ParseException {
+    TimeValue timeValue = TimeValue.parse("09:01:06");
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(6);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the {@code parse} static method with a {@code String} containing minutes.
+   */
+  @Test
+  public void test_parse_containing_minutes() throws ParseException {
+    TimeValue timeValue = TimeValue.parse("09:01");
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(0);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method should throw a {@code NullPointerException} because passing a {@code null} parameter to {@code parse} static method
+   * with a {@code String}.
+   */
+  @Test(expected = NullPointerException.class)
+  public void should_parse_fail_if_date_is_null() throws ParseException {
+    TimeValue.parse(null);
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to {@code parse} static method with a bad length.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_length() throws ParseException {
+    TimeValue.parse("12345678901");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to {@code parse} static method with a bad character on year.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_character_on_year() throws ParseException {
+    TimeValue.parse("a9:01");
+  }
+
+  /**
+   * This method should throw a {@code ParseException} because passing a parameter to {@code parse} static method with a bad character on separator.
+   */
+  @Test(expected = ParseException.class)
+  public void should_parse_fail_if_date_have_bad_character_on_separator() throws ParseException {
+    TimeValue.parse("09a01");
+  }
+
+  /**
+   * This method tests the {@code of} static method with four values.
+   */
+  @Test
+  public void test_of_with_four_values() throws ParseException {
+    TimeValue timeValue = TimeValue.of(9, 1, 6, 3);
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(6);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(3);
+  }
+
+  /**
+   * This method tests the {@code of} static method with three values.
+   */
+  @Test
+  public void test_of_with_three_values() throws ParseException {
+    TimeValue timeValue = TimeValue.of(9, 1, 6);
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(6);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(0);
+  }
+
+  /**
+   * This method tests the {@code of} static method with two values.
+   */
+  @Test
+  public void test_of_with_two_values() throws ParseException {
+    TimeValue timeValue = TimeValue.of(9, 1);
+    assertThat(timeValue.getHour()).isEqualTo(9);
+    assertThat(timeValue.getMinutes()).isEqualTo(1);
+    assertThat(timeValue.getSeconds()).isEqualTo(0);
+    assertThat(timeValue.getNanoSeconds()).isEqualTo(0);
+  }
+
+}
