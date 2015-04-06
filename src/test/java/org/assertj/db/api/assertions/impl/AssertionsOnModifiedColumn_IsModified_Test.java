2fcae9c9ef06fb169142680bd59b3059c6289b41
regis1512 <regis1512@gmail.com>
2015-04-06 11:35:11 +0200

    Add tests on AssertionsOnModifiedColumn

---
+++ src/test/java/org/assertj/db/api/assertions/impl/AssertionsOnModifiedColumn_IsModified_Test.java
@@ -0,0 +1,79 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.db.api.assertions.impl;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.WritableAssertionInfo;
+import org.assertj.db.api.TableAssert;
+import org.assertj.db.type.Table;
+import org.junit.Test;
+
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests on {@link AssertionsOnModifiedColumn} class :
+ * {@link AssertionsOnModifiedColumn#isModified(org.assertj.db.api.AbstractAssert, org.assertj.core.api.WritableAssertionInfo, Object, Object)} method.
+ *
+ * @author Régis Pouiller
+ *
+ */
+public class AssertionsOnModifiedColumn_IsModified_Test {
+
+  /**
+   * This method tests the {@code isOnTable} assertion method.
+   */
+  @Test
+  public void test_is_modified() throws Exception {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    TableAssert tableAssert2 = AssertionsOnModifiedColumn.isModified(tableAssert, info, null, "test");
+    Assertions.assertThat(tableAssert2).isSameAs(tableAssert);
+    tableAssert2 = AssertionsOnModifiedColumn.isModified(tableAssert, info, "test", null);
+    Assertions.assertThat(tableAssert2).isSameAs(tableAssert);
+    tableAssert2 = AssertionsOnModifiedColumn.isModified(tableAssert, info, "test", "test1");
+    Assertions.assertThat(tableAssert2).isSameAs(tableAssert);
+  }
+
+  /**
+   * This method should fail because the column is not modified.
+   */
+  @Test
+  public void should_fail_because_column_is_not_modified() throws Exception {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    info.description("description");
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    try {
+      AssertionsOnModifiedColumn.isModified(tableAssert, info, null, null);
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting :\n"
+                                                      + "  <null>\n"
+                                                      + "is modified but is still:\n"
+                                                      + "  <null>");
+    }
+    try {
+      AssertionsOnModifiedColumn.isModified(tableAssert, info, "test", "test");
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting :\n"
+                                                      + "  <\"test\">\n"
+                                                      + "is modified but is still:\n"
+                                                      + "  <\"test\">");
+    }
+  }
+}
---
+++ src/test/java/org/assertj/db/api/assertions/impl/AssertionsOnModifiedColumn_IsNotModified_Test.java
@@ -0,0 +1,87 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.db.api.assertions.impl;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.WritableAssertionInfo;
+import org.assertj.db.api.TableAssert;
+import org.assertj.db.type.Table;
+import org.junit.Test;
+
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests on {@link AssertionsOnModifiedColumn} class :
+ * {@link AssertionsOnModifiedColumn#isNotModified(org.assertj.db.api.AbstractAssert, org.assertj.core.api.WritableAssertionInfo, Object, Object)} method.
+ *
+ * @author Régis Pouiller
+ *
+ */
+public class AssertionsOnModifiedColumn_IsNotModified_Test {
+
+  /**
+   * This method tests the {@code isOnTable} assertion method.
+   */
+  @Test
+  public void test_is_not_modified() throws Exception {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    TableAssert tableAssert2 = AssertionsOnModifiedColumn.isNotModified(tableAssert, info, null, null);
+    Assertions.assertThat(tableAssert2).isSameAs(tableAssert);
+    tableAssert2 = AssertionsOnModifiedColumn.isNotModified(tableAssert, info, "test", "test");
+    Assertions.assertThat(tableAssert2).isSameAs(tableAssert);
+  }
+
+  /**
+   * This method should fail because the column is not modified.
+   */
+  @Test
+  public void should_fail_because_column_is_not_modified() throws Exception {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    info.description("description");
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    try {
+      AssertionsOnModifiedColumn.isNotModified(tableAssert, info, null, "test");
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting :\n"
+                                                      + "  <null>\n"
+                                                      + "is not modified but is :\n"
+                                                      + "  <\"test\">");
+    }
+    try {
+      AssertionsOnModifiedColumn.isNotModified(tableAssert, info, "test", null);
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting :\n"
+                                                      + "  <\"test\">\n"
+                                                      + "is not modified but is :\n"
+                                                      + "  <null>");
+    }
+    try {
+      AssertionsOnModifiedColumn.isNotModified(tableAssert, info, "test", "test1");
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting :\n"
+                                                      + "  <\"test\">\n"
+                                                      + "is not modified but is :\n"
+                                                      + "  <\"test1\">");
+    }
+  }
+}
