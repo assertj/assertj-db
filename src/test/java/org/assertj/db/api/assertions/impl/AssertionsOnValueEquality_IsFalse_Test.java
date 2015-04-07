27c8fd81af3a0db0f4ec8d382481cd64a78cf780
regis1512 <regis1512@gmail.com>
2015-04-07 20:43:03 +0200

    Add tests on AssertionsOnValueEquality

---
+++ src/test/java/org/assertj/db/api/assertions/impl/AssertionsOnValueEquality_IsFalse_Test.java
@@ -0,0 +1,88 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.db.api.assertions.impl;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.WritableAssertionInfo;
+import org.assertj.db.api.TableAssert;
+import org.assertj.db.type.Table;
+import org.junit.Test;
+
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests on {@link  AssertionsOnValueEquality} class :
+ * {@link  AssertionsOnValueEquality#isFalse(org.assertj.db.api.AbstractAssert, org.assertj.core.api.WritableAssertionInfo, Object)} method.
+ *
+ * @author Régis Pouiller
+ *
+ */
+public class AssertionsOnValueEquality_IsFalse_Test {
+
+  /**
+   * This method tests the {@code isFalse} assertion method.
+   */
+  @Test
+  public void test_is_false() {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    TableAssert tableAssert2 = AssertionsOnValueEquality.isFalse(tableAssert, info, false);
+    Assertions.assertThat(tableAssert2).isSameAs(tableAssert);
+  }
+
+  /**
+   * This method should fail because the value is true.
+   */
+  @Test
+  public void should_fail_because_value_is_true() {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    info.description("description");
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    try {
+      AssertionsOnValueEquality.isFalse(tableAssert, info, true);
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting:\n"
+                                                      + "  <true>\n"
+                                                      + "to be equal to: \n"
+                                                      + "  <false>");
+    }
+  }
+
+  /**
+   * This method should fail because the value is not a boolean.
+   */
+  @Test
+  public void should_fail_because_value_is_not_a_boolean() {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    info.description("description");
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    try {
+      AssertionsOnValueEquality.isFalse(tableAssert, info, 8);
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting:\n"
+                                                      + "  <8>\n"
+                                                      + "to be of type\n"
+                                                      + "  <BOOLEAN>\n"
+                                                      + "but was of type\n"
+                                                      + "  <NUMBER>");
+    }
+  }
+}
---
+++ src/test/java/org/assertj/db/api/assertions/impl/AssertionsOnValueEquality_IsZero_Test.java
@@ -0,0 +1,88 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.db.api.assertions.impl;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.WritableAssertionInfo;
+import org.assertj.db.api.TableAssert;
+import org.assertj.db.type.Table;
+import org.junit.Test;
+
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests on {@link  AssertionsOnValueEquality} class :
+ * {@link  AssertionsOnValueEquality#isZero(org.assertj.db.api.AbstractAssert, org.assertj.core.api.WritableAssertionInfo, Object)} method.
+ *
+ * @author Régis Pouiller
+ *
+ */
+public class AssertionsOnValueEquality_IsZero_Test {
+
+  /**
+   * This method tests the {@code isZero} assertion method.
+   */
+  @Test
+  public void test_is_zero() {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    TableAssert tableAssert2 = AssertionsOnValueEquality.isZero(tableAssert, info, 0);
+    Assertions.assertThat(tableAssert2).isSameAs(tableAssert);
+  }
+
+  /**
+   * This method should fail because the value is not zero.
+   */
+  @Test
+  public void should_fail_because_value_is_not_zero() {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    info.description("description");
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    try {
+      AssertionsOnValueEquality.isZero(tableAssert, info, 1);
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting:\n"
+                                                      + "  <1>\n"
+                                                      + "to be equal to: \n"
+                                                      + "  <0>");
+    }
+  }
+
+  /**
+   * This method should fail because the value is not a number.
+   */
+  @Test
+  public void should_fail_because_value_is_not_a_number() {
+    WritableAssertionInfo info = new WritableAssertionInfo();
+    info.description("description");
+    Table table = new Table();
+    TableAssert tableAssert = assertThat(table);
+    try {
+      AssertionsOnValueEquality.isZero(tableAssert, info, false);
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[description] \n"
+                                                      + "Expecting:\n"
+                                                      + "  <false>\n"
+                                                      + "to be of type\n"
+                                                      + "  <NUMBER>\n"
+                                                      + "but was of type\n"
+                                                      + "  <BOOLEAN>");
+    }
+  }
+}
