f321b575b5ffa107efe52680d49f7b24f076c8fc
regis1512 <regis1512@gmail.com>
2015-04-11 19:53:37 +0200

    Add tests on AssertOnChangeType assertion interface

---
+++ src/test/java/org/assertj/db/api/assertions/AssertOnChangeType_IsCreation_Test.java
@@ -0,0 +1,71 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.db.api.assertions;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.db.api.ChangeAssert;
+import org.assertj.db.common.AbstractTest;
+import org.assertj.db.common.NeedReload;
+import org.assertj.db.type.Changes;
+import org.junit.Test;
+
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests on {@link AssertOnChangeType} class :
+ * {@link AssertOnChangeType#isCreation()}} method.
+ *
+ * @author Régis Pouiller
+ *
+ */
+public class AssertOnChangeType_IsCreation_Test extends AbstractTest {
+
+  /**
+   * This method tests the {@code isCreation} assertion method.
+   */
+  @Test
+  @NeedReload
+  public void test_is_creation() throws Exception {
+    Changes changes = new Changes(source).setStartPointNow();
+    updateChangesForTests();
+    changes.setEndPointNow();
+
+    ChangeAssert changeAssert = assertThat(changes).change();
+    ChangeAssert changeAssert2 = changeAssert.isCreation();
+    Assertions.assertThat(changeAssert).isSameAs(changeAssert2);
+  }
+
+  /**
+   * This method should fail because the type of change is different.
+   */
+  @Test
+  @NeedReload
+  public void should_fail_because_type_of_change_is_different() throws Exception {
+    Changes changes = new Changes(source).setStartPointNow();
+    updateChangesForTests();
+    changes.setEndPointNow();
+
+    try {
+      assertThat(changes).change(3).isCreation();
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[Change at index 3 of Changes on tables of 'sa/jdbc:h2:mem:test' source] \n"
+                                                      + "Expecting:\n"
+                                                      + "to be of type\n"
+                                                      + "  <CREATION>\n"
+                                                      + "but was of type\n"
+                                                      + "  <MODIFICATION>");
+    }
+  }
+}
---
+++ src/test/java/org/assertj/db/api/assertions/AssertOnChangeType_IsModification_Test.java
@@ -0,0 +1,71 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.db.api.assertions;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.db.api.ChangeAssert;
+import org.assertj.db.common.AbstractTest;
+import org.assertj.db.common.NeedReload;
+import org.assertj.db.type.Changes;
+import org.junit.Test;
+
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests on {@link AssertOnChangeType} class :
+ * {@link AssertOnChangeType#isModification()}} method.
+ *
+ * @author Régis Pouiller
+ *
+ */
+public class AssertOnChangeType_IsModification_Test extends AbstractTest {
+
+  /**
+   * This method tests the {@code isModification} assertion method.
+   */
+  @Test
+  @NeedReload
+  public void test_is_modification() throws Exception {
+    Changes changes = new Changes(source).setStartPointNow();
+    updateChangesForTests();
+    changes.setEndPointNow();
+
+    ChangeAssert changeAssert = assertThat(changes).change(3);
+    ChangeAssert changeAssert2 = changeAssert.isModification();
+    Assertions.assertThat(changeAssert).isSameAs(changeAssert2);
+  }
+
+  /**
+   * This method should fail because the type of change is different.
+   */
+  @Test
+  @NeedReload
+  public void should_fail_because_type_of_change_is_different() throws Exception {
+    Changes changes = new Changes(source).setStartPointNow();
+    updateChangesForTests();
+    changes.setEndPointNow();
+
+    try {
+      assertThat(changes).change().isModification();
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[Change at index 0 of Changes on tables of 'sa/jdbc:h2:mem:test' source] \n"
+                                                      + "Expecting:\n"
+                                                      + "to be of type\n"
+                                                      + "  <MODIFICATION>\n"
+                                                      + "but was of type\n"
+                                                      + "  <CREATION>");
+    }
+  }
+}
