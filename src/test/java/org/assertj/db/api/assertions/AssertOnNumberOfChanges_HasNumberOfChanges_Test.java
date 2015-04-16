a237145a9299ca9dc3f44247511e1de9973d7861
regis1512 <regis1512@gmail.com>
2015-04-16 21:21:30 +0200

    Add tests on AssertOnNumberOfChanges assertion method

---
+++ src/test/java/org/assertj/db/api/assertions/AssertOnNumberOfChanges_HasNumberOfChanges_Test.java
@@ -0,0 +1,74 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2014 the original author or authors.
+ */
+package org.assertj.db.api.assertions;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.db.api.ChangesAssert;
+import org.assertj.db.common.AbstractTest;
+import org.assertj.db.common.NeedReload;
+import org.assertj.db.type.Changes;
+import org.assertj.db.type.Request;
+import org.assertj.db.type.Table;
+import org.junit.Test;
+
+import static org.assertj.db.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests on {@link org.assertj.db.api.assertions.AssertOnNumberOfChanges} class :
+ * {@link org.assertj.db.api.assertions.AssertOnNumberOfChanges#hasNumberOfChanges(int)} method.
+ *
+ * @author Régis Pouiller
+ *
+ */
+public class AssertOnNumberOfChanges_HasNumberOfChanges_Test extends AbstractTest {
+
+  /**
+   * This method tests the {@code hasNumberOfChanges} assertion method.
+   */
+  @Test
+  @NeedReload
+  public void test_has_number_of_changes() throws Exception {
+    Table table = new Table(source, "actor");
+    Changes changes = new Changes(table).setStartPointNow();
+    updateChangesForTests();
+    changes.setEndPointNow();
+
+    ChangesAssert changesAssert = assertThat(changes);
+    ChangesAssert changesAssert2 = changesAssert.hasNumberOfChanges(3);
+    Assertions.assertThat(changesAssert).isSameAs(changesAssert2);
+  }
+
+  /**
+   * This method should fail because the number of changes is different.
+   */
+  @Test
+  @NeedReload
+  public void should_fail_because_number_of_changes_is_different() throws Exception {
+    Request request = new Request(source, "select * from actor");
+    Changes changes = new Changes(request).setStartPointNow();
+    updateChangesForTests();
+    changes.setEndPointNow();
+
+    try {
+      assertThat(changes).hasNumberOfChanges(9);
+      fail("An exception must be raised");
+    } catch (AssertionError e) {
+      Assertions.assertThat(e.getMessage()).isEqualTo("[Changes on 'select * from actor' request of 'sa/jdbc:h2:mem:test' source] \n"
+                                                      + "Expecting size (number of changes) to be equal to :\n"
+                                                      + "   <9>\n"
+                                                      + "but was:\n"
+                                                      + "   <4>");
+    }
+  }
+}
