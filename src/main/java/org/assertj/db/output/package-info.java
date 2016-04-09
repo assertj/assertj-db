9ba054d54acac9697704e971c039c86a0cd3dd48
regis1512 <regis1512@gmail.com>
2016-04-09 14:03:26 +0200

    Add a package info

---
+++ src/main/java/org/assertj/db/output/package-info.java
@@ -0,0 +1,27 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2012-2015 the original author or authors.
+ */
+/**
+ * This package contains classes that allows to output the informations about table, request or changes.
+ * <p>Like for assertions, it is possible to navigate between the elements.</p>
+ * <p>This diagram shows the part on the tables and requests (the navigation interfaces are in green and the interfaces used by the navigation are in orange) :</p>
+ * <p>
+ * <img src="../../../../../images/output/diagramOfPartOnOutputOnTableAndRequestWithNavigation.png" alt="diagram with navigation on table and request" height="80%" width="80%" >
+ * </p>
+ * <p>This diagram shows the part on the changes (the navigation interfaces are in green and the interfaces used by the navigation are in orange) :</p>
+ * <p>
+ * <img src="../../../../../images/output/diagramOfPartOnOutputOnChangesWithNavigation.png" alt="diagram with navigation on changes" height="80%" width="80%" >
+ * </p>
+ *
+ * @author Régis Pouiller
+ */
+package org.assertj.db.output;
